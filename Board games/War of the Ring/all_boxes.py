1300fa1db94baa4f80e973b642be7c09cd8abe7a
Alexey Eremin <a.eremin.msu@gmail.com>
2022-11-22 09:14:48 +0100

    Add more WotR boxes

--- Board games/War of the Ring/all_boxes.py
+++ Board games/War of the Ring/all_boxes.py
@@ -10,8 +10,25 @@ unit_box_height = 29
 elves_rohan_box_sections = (34, 43, 70)
 elves_rohan_box_width = 88
 
+gondor_northmen_box_sections = (35, 39, 41)
+gondor_northmen_box_width = 96
+
 elves = boxes.sectioned_box_with_text("Elves", elves_rohan_box_width,
                                       list(zip(section_names, elves_rohan_box_sections)),
                                       unit_box_height, font=free_people_font)
 cq.exporters.export(elves, "elves.stl")
 
+rohan = boxes.sectioned_box_with_text("Rohan", elves_rohan_box_width,
+                                      list(zip(section_names, elves_rohan_box_sections)),
+                                      unit_box_height, font=free_people_font)
+cq.exporters.export(rohan, "rohan.stl")
+
+gondor = boxes.sectioned_box_with_text("Gondor", gondor_northmen_box_width,
+                                       list(zip(section_names, gondor_northmen_box_sections)),
+                                       unit_box_height, font=free_people_font)
+cq.exporters.export(gondor, "gondor.stl")
+
+northmen = boxes.sectioned_box_with_text("The North", gondor_northmen_box_width,
+                                         list(zip(section_names, gondor_northmen_box_sections)),
+                                         unit_box_height, font=free_people_font)
+cq.exporters.export(northmen, "northmen.stl")
