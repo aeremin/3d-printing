b1db58815a7a27b160408d5b68b759de35bd1dc5
Alexey Eremin <a.eremin.msu@gmail.com>
2024-03-19 08:35:15 +0100

    Add diameter testers for 5 and 8mm drill bits

--- Woodworking/drill_size_tester.py
+++ Woodworking/drill_size_tester.py
@@ -1,12 +1,15 @@
 import cadquery as cq
 
-sizes = iter([15.0, 15.1, 15.2, 15.3, 15.4])
+def drill_diameter_tester(diameter):
+    sizes = iter([diameter, diameter + 0.1, diameter + 0.2, diameter + 0.3, diameter + 0.4])
+    u = diameter + 5
+    return (cq.Workplane().box(5 * u, u, 6).faces(">Z").workplane()
+         .rarray(u, 1, 5, 1)
+         .eachpoint(
+            lambda loc: cq.Workplane().cylinder(12, next(sizes) / 2).val().located(loc),
+            combine="cut"
+         ))
 
-r = (cq.Workplane().box(100, 20, 6).faces(">Z").workplane()
-     .rarray(20, 1, 5, 1)
-     .eachpoint(
-        lambda loc: cq.Workplane().cylinder(12, next(sizes) / 2).val().located(loc),
-        combine="cut"
-     ))
-
-cq.exporters.export(r, '15mm_0.1_step.stl')
+cq.exporters.export(drill_diameter_tester(5), '5mm_0.1_step.stl')
+cq.exporters.export(drill_diameter_tester(8), '8mm_0.1_step.stl')
+cq.exporters.export(drill_diameter_tester(15), '15mm_0.1_step.stl')
