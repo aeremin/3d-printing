b4e1bad56f95f67490fe1902d6622e03fe0955a7
Alexey Eremin <a.eremin.msu@gmail.com>
2024-03-19 08:34:56 +0100

    Add holder for larger knife blades

--- Apartment/Gridfinity/stanley_knife.py
+++ Apartment/Gridfinity/stanley_knife.py
@@ -15,4 +15,19 @@ def blades_holder_9mm():
            )
     return box
 
+def blades_holder_18mm():
+    solid_thickness = 5
+    height_u = 3
+    box = GridfinityBox(3, 1, 3, holes=True, unsupported_holes=True, solid=True,
+                        solid_ratio=solid_thickness / (height_u * constants.GRHU)).cq_obj
+    l = 121
+    h = 25
+    box = (box.faces("<Z[1]").workplane(invert=True)
+           .rect(l, h).cutBlind(solid_thickness)
+           .rect(l, h).cutBlind(-height_u * constants.GRHU)
+           )
+    return box
+
+
 cq.exporters.export(blades_holder_9mm(), 'blades_holder_9mm.stl')
+cq.exporters.export(blades_holder_18mm(), 'blades_holder_18mm.stl')
