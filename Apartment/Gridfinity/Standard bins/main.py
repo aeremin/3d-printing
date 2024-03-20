e4018d6b2100685cdfef28cc6446ca8122f9a371
Alexey Eremin <a.eremin.msu@gmail.com>
2024-03-20 21:44:04 +0100

    Add bunch of small generic bins

---
+++ Apartment/Gridfinity/Standard bins/main.py
@@ -0,0 +1,11 @@
+from cqgridfinity import *
+
+sizes = [
+    (1, 1, 6),
+    (1, 2, 6),
+    (2, 1, 6),
+    (2, 2, 6)
+]
+
+for l, w, h in sizes:
+    GridfinityBox(l, w, h, scoops=True, labels=True, holes=True, unsupported_holes=True).save_stl_file()
