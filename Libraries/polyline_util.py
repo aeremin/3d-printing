8242630ab13d33cd8ba22b851b2ff35f6c49b0e4
Alexey Eremin <a.eremin.msu@gmail.com>
2024-03-22 22:43:58 +0100

    Support changing starting point in polyline_util

--- Libraries/polyline_util.py
+++ Libraries/polyline_util.py
@@ -4,9 +4,10 @@ from typing import List, Tuple
 class PolylineFromOffsets:
     points: List[Tuple[float, float]]
 
-    def __init__(self):
-        self.points = [(0, 0)]
+    def __init__(self, start_point: Tuple[float, float] = (0, 0)):
+        self.points = [start_point]
 
     def add_point(self, x: float, y: float):
         last_x, last_y = self.points[-1]
         self.points.append((last_x + x, last_y + y))
+        return self
