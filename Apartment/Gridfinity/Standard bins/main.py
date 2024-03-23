368021b2f713260b95b5bb79dd108fad45f3bf69
Alexey Eremin <a.eremin.msu@gmail.com>
2024-03-23 22:05:22 +0100

    Add more standard boxes

--- Apartment/Gridfinity/Standard bins/main.py
+++ Apartment/Gridfinity/Standard bins/main.py
@@ -4,9 +4,11 @@ sizes = [
     (1, 1, 6),
     (1, 2, 6),
     (2, 1, 6),
-    (2, 2, 6)
+    (2, 2, 6),
+    (1, 1, 3),
 ]
 
 for l, w, h in sizes:
     GridfinityBox(l, w, h, scoops=True, labels=True, holes=True, unsupported_holes=True).save_stl_file()
     GridfinityBox(l, w, h, scoops=True, holes=True, unsupported_holes=True).save_stl_file()
+
