87bc6a75228bef81e0f1db27f4220aa2f864d7be
Alexey Eremin <a.eremin.msu@gmail.com>
2024-03-26 23:12:43 +0100

    Add more standard bins

--- Apartment/Gridfinity/Standard bins/main.py
+++ Apartment/Gridfinity/Standard bins/main.py
@@ -1,11 +1,18 @@
 from cqgridfinity import *
 
+GridfinityBox(1, 3, 4, scoops=False, labels=False, holes=True, unsupported_holes=True).save_stl_file()
+GridfinityBox(1, 4, 5, scoops=False, labels=False, holes=True, unsupported_holes=True).save_stl_file()
+GridfinityBox(1, 4, 7, scoops=False, labels=False, holes=True, unsupported_holes=True).save_stl_file()
+GridfinityBox(1, 1, 2, length_div=1, scoops=True, labels=True, holes=True, unsupported_holes=True).save_stl_file()
+
 sizes = [
     (1, 1, 6),
     (1, 2, 6),
     (2, 1, 6),
     (2, 2, 6),
     (1, 1, 3),
+    (1, 1, 2),
+    (1, 3, 3),
 ]
 
 for l, w, h in sizes:
