6c035357e8f74dedf50306fb2c6ef0929902e2df
Alexey Eremin <a.eremin.msu@gmail.com>
2024-07-04 00:19:05 +0200

    Add spool holder for a1 mini

---
+++ 3D Printer/a1mini_containerized.py
@@ -0,0 +1,31 @@
+import cadquery as cq
+
+hole_width = 21
+hole_thickness = 9.5
+hole_depth = 28
+
+spool_holder_diameter = hole_width
+spool_holder_length = 80
+
+spool_holder_hole_side = 10
+spool_holder_hole_depth = 10
+
+stopper_thickness = 5
+stopper_diameter = 40
+
+spool_holder_base = cq.Workplane().box(hole_width, hole_depth + spool_holder_diameter / 2, hole_thickness,
+                                       centered=(True, False, False))
+spool_holder = (
+  cq.Workplane(origin=(0, hole_depth + spool_holder_diameter / 2, 0))
+  .cylinder(spool_holder_length, spool_holder_diameter / 2, centered=(True, True, False))
+  .faces(">Z").workplane().rect(spool_holder_hole_side, spool_holder_hole_depth).cutBlind(-spool_holder_hole_depth)
+).union(spool_holder_base)
+
+cq.exporters.export(spool_holder, 'a1mini_spool_holder.stl')
+
+eps = 0.15
+stopper = (cq.Workplane().cylinder(stopper_thickness, stopper_diameter / 2)
+           .faces(">Z").workplane()
+           .rect(spool_holder_hole_side - eps, spool_holder_hole_side - eps).extrude(spool_holder_hole_depth - eps))
+
+cq.exporters.export(stopper, 'a1mini_spool_holder_stopper.stl')
