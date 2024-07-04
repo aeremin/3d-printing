ff3046958d7e022e877e4a545a7329efaa9662a5
Alexey Eremin <a.eremin.msu@gmail.com>
2024-07-04 22:10:25 +0200

    More objects for skydiving plane cover

--- Sports/skydiving_plane_cover.py
+++ Sports/skydiving_plane_cover.py
@@ -1,9 +1,12 @@
 import cadquery as cq
 
-INNER_DIAMETER = 4.2
+METAL_HOLE_DIAMETER = 4.8 - 0.2
+ROD_THICKNESS = 2.9 + 0.2
 OUTER_DIAMETER = 8
 LENGTH = 254 / (10 + 10)
 
-r = cq.Workplane().cylinder(LENGTH, OUTER_DIAMETER / 2).faces(">Z").hole(INNER_DIAMETER)
+outer_cylinder = cq.Workplane().cylinder(LENGTH, OUTER_DIAMETER / 2).faces(">Z").hole(ROD_THICKNESS)
+inner_cylinder = cq.Workplane().cylinder(LENGTH, METAL_HOLE_DIAMETER / 2).faces(">Z").hole(ROD_THICKNESS)
 
-cq.exporters.export(r, 'sample_4.stl')
+cq.exporters.export(outer_cylinder, 'skydiving_plane_outer_cylinder.stl')
+cq.exporters.export(inner_cylinder, 'skydiving_plane_inner_cylinder.stl')
